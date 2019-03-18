f79957b3504b5f0e4c0d50140c6191f24f9816a9
Gleb Kalachev <megas781@gmail.com>
2019-03-18 03:29:05 +0300

    Удалён Firebase. Работа продолжается

--- models/CategoriesModel.php
+++ models/CategoriesModel.php
@@ -11,7 +11,6 @@ class CategoriesModel
     public static function getCategoriesList()
     {
 
-
         $pdo = Database::getPdoConnection();
         $categories = $pdo->query("select category_id, category_name, status from mvc_shop_categories order by sort_order asc ")->fetchAll();
 
---
+++ models/ProductsModel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: glebkalachev
+ * Date: 2019-03-18
+ * Time: 01:51
+ */
+
+class ProductsModel
+{
+
+    public static function getProductsList()
+    {
+
+    }
+}
